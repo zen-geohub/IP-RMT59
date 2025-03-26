acac71b26c912d6a6f4324121143b7e2a3ee2b4a
zen-geohub <abidinzaenal556@gmail.com>
2025-03-26 21:28:31 +0700

    feat: login register

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import LoginPage from "./pages/LoginPage";
 import Dashboard from "./pages/Dashboard";
 import MainLayout from "./layouts/MainLayout";
 import UserFavoritePage from "./pages/UserFavoritePage";
+import RegisterPage from "./pages/RegisterPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route element={<MainLayout />}>
           <Route path="/" element={<Dashboard />} />
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,51 +1,16 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import http from "../helpers/http";
 import { NavLink, useNavigate } from "react-router";
-import { IoKey, IoMail } from "react-icons/io5";
+import { IoKey, IoMail, IoPersonSharp } from "react-icons/io5";
 
-const LoginPage = () => {
+const RegisterPage = () => {
   const [user, setUser] = useState({
     email: "",
     password: "",
+    firstName: "",
+    lastName: "",
   });
   const navigate = useNavigate();
-
-  useEffect(() => {
-    if (localStorage.getItem("access_token")) {
-      navigate("/");
-    }
-  }, []);
-
-  async function handleCredentialResponse(response) {
-    try {
-      const { data } = await http({
-        method: "POST",
-        url: "/google-login",
-        data: {
-          token: response.credential,
-        },
-      });
-
-      localStorage.setItem("access_token", data?.access_token);
-      localStorage.getItem("access_token") !== undefined
-        ? navigate("/")
-        : navigate("/login");
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  useEffect(() => {
-    // eslint-disable-next-line no-undef
-    google.accounts.id.initialize({
-      client_id: import.meta.env.VITE_GOOGLE_CLIENT_ID,
-      callback: handleCredentialResponse,
-    });
-    // eslint-disable-next-line no-undef
-    google.accounts.id.renderButton(
-      document.getElementById("googleBtn"),
-      { theme: "outline", size: "large" } // customization attributes
-    );
-  }, []);
 
   return (
     <form
@@ -53,16 +18,13 @@ const LoginPage = () => {
         try {
           e.preventDefault();
 
-          const { data } = await http({
+          await http({
             method: "POST",
-            url: "/login",
+            url: "/register",
             data: user,
           });
 
-          localStorage.setItem("access_token", data?.access_token);
-          localStorage.getItem("access_token") !== undefined
-            ? navigate("/")
-            : navigate("/login");
+          navigate("/login");
         } catch (error) {
           console.error(error);
         }
@@ -70,6 +32,34 @@ const LoginPage = () => {
       className="w-full h-dvh flex justify-center items-center"
     >
       <div className="flex flex-col bg-gray-50 p-2 rounded-xl">
+        <label className="input floating-label mb-2">
+          <span>First Name</span>
+          <IoPersonSharp className="text-gray-500" />
+          <input
+            type="text"
+            required
+            placeholder="First Name"
+            name="firstName"
+            onChange={(e) =>
+              setUser((value) => ({ ...value, firstName: e.target.value }))
+            }
+          />
+        </label>
+
+        <label className="input floating-label mb-2">
+          <span>Last Name</span>
+          <IoPersonSharp className="text-gray-500" />
+          <input
+            type="text"
+            required
+            placeholder="Last Name"
+            name="lastName"
+            onChange={(e) =>
+              setUser((value) => ({ ...value, lastName: e.target.value }))
+            }
+          />
+        </label>
+
         <label className="input floating-label mb-2">
           <span>Email</span>
           <IoMail className="text-gray-500" />
@@ -99,14 +89,14 @@ const LoginPage = () => {
         </label>
 
         <button className="btn btn-primary mb-2" type="submit">
-          Login
+          Sign Up
         </button>
         <div id="googleBtn"></div>
 
         <p className="text-xs mt-2 text-center">
-          Don't have account?{" "}
-          <NavLink to={"/register"} className={"font-bold text-primary"}>
-            Sign up.
+          Already have account?{" "}
+          <NavLink to={"/login"} className={"font-bold text-primary"}>
+            Sign in.
           </NavLink>
         </p>
       </div>
@@ -114,4 +104,4 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+export default RegisterPage;
